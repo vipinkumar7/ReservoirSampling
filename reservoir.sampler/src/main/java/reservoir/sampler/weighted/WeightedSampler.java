35a4902dd3b7def6b21149e3a5729fade7c5af29
vipinkumar7 <vipinkumar.work@gmail.com>
2017-10-15 23:24:57 +0530

    add random string generator

--- reservoir.sampler/src/main/java/reservoir/sampler/weighted/WeightedSampler.java
+++ reservoir.sampler/src/main/java/reservoir/sampler/weighted/WeightedSampler.java
@@ -3,6 +3,9 @@ package reservoir.sampler.weighted;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
+import java.util.Base64;
 import java.util.PriorityQueue;
 import java.util.Random;
 
@@ -20,6 +23,16 @@ import org.apache.logging.log4j.Logger;
 public class WeightedSampler {
 
     final static Logger logger = LogManager.getLogger(WeightedSampler.class);
+
+    public static void generate(int num_char) {
+
+	SecureRandom random = new SecureRandom();
+	byte[] values = new byte[num_char];
+	random.nextBytes(values);
+	String randomVal = new String(Base64.getEncoder().encode(values),
+		StandardCharsets.UTF_8);
+	System.out.println(randomVal);
+    }
 
     public static String create(int reservoir_size, boolean is_random_seed) {
 	PriorityQueue<Tuple> minHeap = new PriorityQueue<Tuple>(reservoir_size,
@@ -72,6 +85,10 @@ public class WeightedSampler {
 	optionParser.accepts("sample_size").withRequiredArg()
 		.ofType(Integer.class).describedAs("Sample size for output")
 		.defaultsTo(5);
+	optionParser.accepts("generate").withRequiredArg()
+		.ofType(Integer.class)
+		.describedAs("Generate a random string of length n ")
+		.defaultsTo(20);
 	optionParser.accepts("h", "show help").forHelp();
 
 	int sample_size = 5;
@@ -84,7 +101,11 @@ public class WeightedSampler {
 			.toString());
 	    if (options.has("random_seed"))
 		random_seed = true;
-	    if (!options.has("h"))
+	    if (options.has("generate")) {
+		int num_char = Integer.parseInt(options.valueOf("generate")
+			.toString());
+		generate(num_char);
+	    } else if (!options.has("h"))
 		System.out.println(create(sample_size, random_seed));
 	} catch (NumberFormatException nfe) {
 	    logger.info("first argument is sample size , it shoud be integer ");
